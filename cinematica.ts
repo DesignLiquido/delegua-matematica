86a554fe45da1d29d92ae89c91a9ffa4b2cfe97b
Samuel Renan Gonçalves Vaz <samuel.rvg@gmail.com>
2022-01-10 16:07:22 -0400

    Tipagem e alguns ajustes (#3)

--- cinematica.ts
+++ cinematica.ts
@@ -1,79 +1,32 @@
-import { MatematicaErroEmTempoDeExecucao } from "./excecoes";
-
 //Velocidade média
-export function velocidadeMedia(s, t) {
-  if (isNaN(s) || s === null || isNaN(t) || t === null)
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Você deve prover valores para velocidadeMedia(d,t)."
-    );
-
-  return s / t;
+export function velocidadeMedia(intervaloDeslocamento: number, intervaloTempo: number): number {
+  return intervaloDeslocamento / intervaloTempo;
 }
 
 //Espaço percorrido
-export function deltaS(s0, s) {
-  if (isNaN(s0) || s0 === null || isNaN(s) || s === null)
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Você deve prover valores para deltas(e0,e1)."
-    );
+export function deltaS(s0: number, s: number): number {
   const ds = s - s0;
   return ds;
 }
 
 //Tempo Percorrido
-export function deltaT(t0, t) {
-  if (isNaN(t0) || t0 === null || isNaN(t) || t === null)
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Você deve prover valores para deltat(t0,t1)."
-    );
+export function deltaT(t0: number, t: number): number {
   const dt = t - t;
   return dt;
 }
 
 // Cálculo de aceleração
 export function aceleracao(
-  velocidadeFinal,
-  velocidadeInicial,
-  tempoFinal,
-  tempoInicial
-) {
-  if (
-    velocidadeFinal === null ||
-    velocidadeInicial === null ||
-    tempoFinal === null ||
-    tempoInicial === null
-  ) {
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Devem ser fornecidos quatro parâmetros obrigatórios."
-    );
-  }
-
-  if (
-    typeof velocidadeFinal !== "number" ||
-    typeof velocidadeInicial !== "number" ||
-    typeof tempoFinal !== "number" ||
-    typeof tempoInicial !== "number"
-  ) {
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Todos os parâmetros devem ser do tipo número."
-    );
-  }
-
+  velocidadeFinal: number,
+  velocidadeInicial: number,
+  tempoFinal: number,
+  tempoInicial: number
+): number {
   return (velocidadeFinal - velocidadeInicial) / (tempoFinal - tempoInicial);
 }
 
 //Função Horária da Posição (M.R.U)
-export function mrufh(s0, v, t) {
-  if (isNaN(s0) || s0 === null)
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Você deve prover valores para mrufh(s0,v,t)."
-    );
+export function mrufh(s0: number, v: number, t: number): Array<string> {
   t = t + 1;
   const s = new Array();
   let index = 0;
@@ -86,19 +39,7 @@ export function mrufh(s0, v, t) {
 }
 
 //Gráfico da velocidade (M.R.U.V)
-export function mruv(s0, s, a) {
-  if (
-    isNaN(s0) ||
-    s0 === null ||
-    isNaN(s) ||
-    s === null ||
-    isNaN(a) ||
-    a === null
-  )
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Você deve prover valores para mruv(Pi, Vf, A)."
-    );
+export function mruv(s0: number, s: number, a: number): Array<any> {
   const vf = new Array();
   const x = new Array();
   let v = new Array();
--- index.ts
+++ index.ts
@@ -1,13 +1,5 @@
-import { MatematicaErroEmTempoDeExecucao } from "./excecoes";
-
 //Mediana de uma matriz
-export function mediana(a) {
-  if (isNaN(a) || a === null)
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Você deve prover valores para mediana(a)."
-    );
-
+export function mediana(a: Array<any>): any {
   a.sort(function (a, b) { return a - b; });
   const mid = a.length / 2;
   return mid % 1 ? a[mid - 0.5] : (a[mid - 1] + a[mid]) / 2;
@@ -20,22 +12,10 @@ export function mediana(a) {
  * @returns O novo conjunto com os valores da moda.
  * @see https://pt.wikipedia.org/wiki/Moda_(estat%C3%ADstica)
  */
-export function moda(vetor) {
-  if (!Array.isArray(vetor))
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Parâmetro `vetor` deve ser um vetor, em min(vetor)."
-    );
-
-  if (vetor.some(isNaN))
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Todos os elementos de `vetor` deve ser numéricos, em min(vetor)."
-    );
-
+export function moda(vetor: Array<number>): any {
   const objectArr = vetor.reduce(
-    function (acc, curr) { 
-      return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc 
+    function (acc, curr) {
+      return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc
     },
     {}
   )
@@ -44,31 +24,23 @@ export function moda(vetor) {
     counter.push(objectArr[pos])
   })
   const max = Math.max.apply(null, counter)
-  
+
   if (max === 1) {
     return []
   }
 
   return Object.keys(objectArr).filter(function (pos) {
-    return objectArr[pos] === max 
+    return objectArr[pos] === max
       ? objectArr[pos]
       : null
   }).map(item => Number(item))
 }
-/**
- * Função que sempre returna `nulo`. 
- * Útil para comparações entre outras funções que também retornam nulo.
- * @returns `null` do JavaScript.
- */
-export function nula() {
-  return null;
-};
 
 /**
  * Constante pi.
  * @see https://pt.wikipedia.org/wiki/Pi
  */
-export function pi () { return Math.PI }
+export function pi(): number { return Math.PI }
 
 /**
  * Calcula o valor radiano de um ângulo. O radiano é o comprimento do arco formado 
@@ -77,13 +49,7 @@ export function pi () { return Math.PI }
  * @returns O valor, em radianos, do arco formado pelo ângulo.
  * @see https://pt.wikipedia.org/wiki/Radiano
  */
-export function radiano(angulo) {
-  if (!Number.isInteger(angulo))
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Você deve prover um número inteiro para o parâmetro `angulo`, em radiano(angulo)."
-    );
-
+export function radiano(angulo: number): number {
   return angulo * (Math.PI / 180);
 };
 
@@ -96,53 +62,27 @@ export function radiano(angulo) {
  * @returns Um vetor, contendo o número de pontos informado ou definido por padrão em uma abscissa.
  *          Se o número informado é par, um ponto negativo a mais é gerado.
  */
-export function gerarPontosAbscissa(distancia, valorPontoCentral, numeroPontos) {
-  if (!Number.isInteger(distancia))
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Você deve prover um valor inteiro para o parâmetro `distancia`, em gerarPontosAbscissa(distancia, valorInicial)."
-    );
-
-  if (!Number.isInteger(valorPontoCentral))
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Você deve prover um valor inteiro para o parâmetro `valorInicial`, em gerarPontosAbscissa(distancia, valorInicial)."
-    );
-
+export function gerarPontosAbscissa(distancia: number, valorPontoCentral: number, numeroPontos?: number) {
   if (!numeroPontos) {
     numeroPontos = 7;
   }
 
   const elementoInicial = valorPontoCentral - (((numeroPontos / 2) >> 0) * distancia);
-  const x = [];
+  const vetor = [];
   for (let i = 0; i < numeroPontos; i++) {
-    x.push(elementoInicial + (i * distancia));
+    vetor.push(elementoInicial + (i * distancia));
   }
 
-  return x;
+  return vetor;
 };
 
 //Raíz da Função Afim
-export function fun1R(a, b) {
-  if (isNaN(a) || a === null || isNaN(b) || b === null)
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Você deve prover valores para fun1R(valor1,valor2)."
-    );
+export function fun1R(a: number, b: number) {
   return (-1 * b) / a;
 };
 
 //Intervalo Preenchido
-export function linspace(startValue, stopValue, cardinality) {
-  if (
-    isNaN(startValue) || startValue === null ||
-    isNaN(stopValue) || stopValue === null ||
-    isNaN(cardinality) || cardinality === null
-  )
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Você deve prover valores para linspace(valor1,valor2,valor3)."
-    );
+export function linspace(startValue: number, stopValue: number, cardinality: number): Array<any> {
   const lista = [];
   const step = (stopValue - startValue) / (cardinality - 1);
   for (var i = 0; i < cardinality; i++) {
@@ -152,15 +92,10 @@ export function linspace(startValue, stopValue, cardinality) {
 };
 
 //Raízes da Função Quadrática
-export function fun2R(a, b, c) {
-  if (isNaN(a) || a === null)
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Você deve prover valores para fun2R(a,b,c)."
-    );
+export function fun2R(a: number, b: number, c: number): Array<any> {
 
-  const r1 = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
-  const r2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+  // const r1 = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+  // const r2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
 
   const xv = (-1 * b) / (2 * a);
   const yv = (-1 * (Math.pow(b, 2) - (4 * a * c))) / 4 * a;
@@ -169,12 +104,7 @@ export function fun2R(a, b, c) {
 };
 
 //Aproximação de valores
-export function aprox(x, z) {
-  if (isNaN(x) || x === null || isNaN(z) || z === null)
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Você deve prover valores para aprox(x,z)."
-    );
+export function aprox(x: any, z?: number): any {
   if (z == undefined) { z = 2; }
   if (typeof (x) == "number") { x = x.toFixed(z) }
   else if (x[0].length == undefined) { // 1D array
@@ -191,21 +121,16 @@ export function aprox(x, z) {
 };
 
 //Vetor de pontos aleatórios
-export function pontosAleatorios(n) {
-  if (isNaN(n) || n === null)
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Você deve prover valores para pale(n)."
-    );
+export function pontosAleatorios(valor: number): any {
   let ex;
   if (ex == undefined) { ex = 0; }
   const x = [];
   x[0] = 100;
-  for (let i = 1; i < n; i++) {
+  for (let i = 1; i < valor; i++) {
     x[i] = ex + x[i - 1] + Math.random() * 2 - 1;
   }
-  const xx = aprox(x, 2);
-  return xx;
+
+  return aprox(x, 2);
 };
 
 
@@ -215,83 +140,36 @@ export function pontosAleatorios(n) {
  * @param {qualquer} valor Valor a ser encontrado no vetor
  * @returns Valor inteiro, com o número de vezes que `valor` foi encontrado em `vetor`.
  */
-export function numeroOcorrencias(vetor, valor) {
-  if (!Array.isArray(vetor))
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Parâmetro `vetor` deve ser um vetor, em numeroOcorrencias(vetor, valor)."
-    );
-
+export function numeroOcorrencias(vetor: Array<any>, valor: number): number {
   return vetor.filter((v) => (v === valor)).length;
 };
 
 //Exponencial
-export function exp(x) {
-  if (isNaN(x) || x === null)
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Você deve prover valores para exp(x)."
-    );
-
-  return Math.exp(x);
+export function exp(valor: number): number {
+  return Math.exp(valor);
 };
 
 //Logaritmo natural
-export function log(x) {
-  if (isNaN(x) || x === null)
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Você deve prover valores para log(x)."
-    );
-
-  return Math.log(x);
+export function log(valor: number): number {
+  return Math.log(valor);
 };
 
 // Retorna a base elevada ao expoente
-export function potencia(base, expoente) {
-  if (typeof base !== 'number' || typeof expoente !== 'number') {
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Os parâmetros devem ser do tipo número."
-    );
-  }
-
+export function potencia(base: number, expoente: number): number {
   return Math.pow(base, expoente);
 };
 
 //Raíz quadrada
-export function raizq(x) {
-  if (isNaN(x) || x === null)
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "Você deve prover valores para raizq(x)."
-    );
-
-  return Math.sqrt(x);
+export function raizQuadrada(valor: number): number {
+  return Math.sqrt(valor);
 };
 
 // Retorna o comprimento de um vetor
-export function comp(array) {
-
-  if (!Array.isArray(array)) {
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "O valor passado pra função deve ser um vetor."
-    );
-  }
-
-  return array.length;
+export function comprimentoVetor(vetor: Array<any>): number {
+  return vetor.length;
 };
 
 // Retorna o menor número inteiro dentre o valor de "value"
-export function minaprox(value) {
-
-  if (typeof value !== 'number') {
-    throw new MatematicaErroEmTempoDeExecucao(
-      this.token,
-      "O valor passado pra função deve ser um número."
-    );
-  }
-
+export function minimoAproximado(value: number): number {
   return Math.floor(value);
 };
